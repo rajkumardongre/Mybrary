7c695dcf7891103ca99f0abb3dce300c74bf4d16
rajkumardongre <rajkumardongre17@gmail.com>
2021-09-15 19:07:34 +0530

    Initial Author Routes

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ if (process.env.NODE_ENV !== "production") {
 const { escapeXML } = require("ejs");
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -15,6 +18,7 @@ app.set("views", `${__dirname}/views`);
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
 const url = process.env.DATABASE_URL;
@@ -24,6 +28,7 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
